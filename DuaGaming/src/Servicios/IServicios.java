c0cd10bbbd034d9bafe68e8dcf35ab8086a01c6f
admin <admin@pcenterjava-14>
2018-08-28 15:32:15 +0200

    28/08/2018 15:31 Vitu
    Creación de la capa de servicios con interfaz IServicios y clase
    Servicios

---
+++ DuaGaming/src/Servicios/IServicios.java
@@ -0,0 +1,12 @@
+package Servicios;
+
+import java.util.ArrayList;
+
+public interface IServicios {
+	
+	public Juego getJuego(int id);
+	
+	public ArrayList<Juego> getJuegosTitulo(String titulo);
+	
+		
+}
---
+++ DuaGaming/src/Servicios/Servicios.java
@@ -0,0 +1,20 @@
+package Servicios;
+
+import java.util.ArrayList;
+
+public class Servicios implements IServicios {
+	
+	@Override
+	public Juego getJuego(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Juego> getJuegosTitulo(String titulo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
